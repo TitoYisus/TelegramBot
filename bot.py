bdf84927565b1cfec4b38eb55b38f754f0a85207
Jesus Ruslan Esquinas Herrera <jesesqher@alum.us.es>
2023-09-19 08:23:38 +0200

    prueba gitignore

--- bot.py
+++ bot.py
@@ -13,7 +13,6 @@ def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
 
 
-
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
