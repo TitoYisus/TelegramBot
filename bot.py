5d4923188e8c336c2ad6aa3eeee17d092f69f8e2
Jesus Ruslan Esquinas Herrera <jesesqher@alum.us.es>
2023-09-19 08:20:50 +0200

    actualización gitignore

--- bot.py
+++ bot.py
@@ -12,6 +12,8 @@ bot = telebot.TeleBot(BOT_TOKEN)
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
 
+
+
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
