9bb9b19c2f119d4ae411fa1b4426a8900dc617cd
Jesús Ruslan Esquinas Herrera <jesesqher@alum.us.es>
2023-09-22 19:18:31 +0200

    Code Formatting Final

--- bot.py
+++ bot.py
@@ -10,54 +10,67 @@ from collections import namedtuple
 from datetime import date, datetime
 
 load_dotenv()
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 bot = telebot.TeleBot(BOT_TOKEN)
 
 
-@bot.message_handler(commands=['ayuda'])
+@bot.message_handler(commands=["ayuda"])
 def send_help(message):
     bot.reply_to(message, "")
 
-@bot.message_handler(commands=['anadir'])
+
+@bot.message_handler(commands=["anadir"])
 def add_dates(message):
     cid = message.chat.id
 
-    bot.send_message(chat_id=cid, text="Envíame un csv (con caption '/s') separado por punto y coma (;) " + 
-                     "con los recordatorios definidos en las siguientes columnas:\n\n1. \"fecha\": " +
-                     "Fecha en la que recordar (formato DD/MM/YYYY)\n2. \"mensaje\": mensaje que " +
-                     "quieras que se envíe\n3. \"user\": usuarios a los que mencionar separados por " +
-                     "comas sin espacios [OPCIONAL]\n\nAsegúrate de guardarlo como CSV UTF-8\n\n" +
-                     "PREGUNTA A ALGUIEN POR EL GRUPO SI TIENES ALGUNA DUDA")
-    
-    local_photo_path = './files/ejemplo.jpg'
-    mensajito = "ejemplo de excel antes de guardarlo como csv"
-    bot.send_photo(chat_id=cid, photo=open(local_photo_path, 'rb'), caption=mensajito)
-    local_photo_path = './files/tipo_csv.jpg'
-    mensajito = "opción de tipo que seleccionar en \"guardar como\""
-    bot.send_photo(chat_id=cid, photo=open(local_photo_path, 'rb'), caption=mensajito)
+    bot.send_message(
+        chat_id=cid,
+        text="Envíame un csv (con caption '/s') separado por punto y coma (;) "
+        + 'con los recordatorios definidos en las siguientes columnas:\n\n1. "fecha": '
+        + 'Fecha en la que recordar (formato DD/MM/YYYY)\n2. "mensaje": mensaje que '
+        + 'quieras que se envíe\n3. "user": usuarios a los que mencionar separados por '
+        + "comas sin espacios [OPCIONAL]\n\nAsegúrate de guardarlo como CSV UTF-8\n\n"
+        + "PREGUNTA A ALGUIEN POR EL GRUPO SI TIENES ALGUNA DUDA",
+    )
 
-@bot.message_handler(func=lambda message: message.document.mime_type == 'text/csv', content_types=['document'])
+    local_photo_path = "./files/ejemplo.jpg"
+    mensajito = "ejemplo de excel antes de guardarlo como csv"
+    bot.send_photo(chat_id=cid, photo=open(local_photo_path, "rb"), caption=mensajito)
+    local_photo_path = "./files/tipo_csv.jpg"
+    mensajito = 'opción de tipo que seleccionar en "guardar como"'
+    bot.send_photo(chat_id=cid, photo=open(local_photo_path, "rb"), caption=mensajito)
+
+
+@bot.message_handler(
+    func=lambda message: message.document.mime_type == "text/csv",
+    content_types=["document"],
+)
 def actualizar(message):
     if message.caption == "/s":
         descargar_csv(message)
         diccionario = parse_recordatorios(message)
         actualizar_json(message, diccionario)
     else:
-        bot.reply_to(message, "Veo que has mandado un .csv, si quieres que me lo descargue " +
-                     "actualizar los recordatorios asegúrate de ponerle el comando '/s' como caption")
+        bot.reply_to(
+            message,
+            "Veo que has mandado un .csv, si quieres que me lo descargue "
+            + "actualizar los recordatorios asegúrate de ponerle el comando '/s' como caption",
+        )
+
 
 def descargar_csv(message):
     cid = message.chat.id
     nombre = message.document.file_name
     bot.reply_to(message, "Descargando " + nombre + "...")
-    
+
     id_archivo = message.document.file_id
     file_link = bot.get_file_url(id_archivo)
 
     file_path = "./files/recordatorios.csv"
     response = requests.get(file_link)
-    with open(file_path, 'wb') as recordatorios:
+    with open(file_path, "wb") as recordatorios:
         recordatorios.write(response.content)
+
 
 def parse_recordatorios(message):
     cid = message.chat.id
@@ -65,8 +78,8 @@ def parse_recordatorios(message):
 
     file_path = "./files/recordatorios.csv"
     recordatorios = []
-    with open(file_path, encoding='utf-8') as fichero:
-        lector = csv.reader(fichero, delimiter=';')
+    with open(file_path, encoding="utf-8") as fichero:
+        lector = csv.reader(fichero, delimiter=";")
         next(lector)
         for linea in lector:
             fecha_hoy = date.today()
@@ -78,13 +91,15 @@ def parse_recordatorios(message):
 
                 tupla = (fecha, texto, usuarios)
                 recordatorios.append(tupla)
-    
+
     res = ordena_por_fecha(recordatorios)
     return res
+
 
 def parsea_usuarios(usuarios):
     lista_usuarios = usuarios.split(",")
     return lista_usuarios
+
 
 def ordena_por_fecha(recordatorios):
     res = dict()
@@ -97,12 +112,15 @@ def ordena_por_fecha(recordatorios):
             res[r[0]].append(r)
     return res
 
+
 def actualizar_json(message, recordatorios):
     cid = message.chat.id
-    bot.send_message(chat_id=cid, text="Parseo finalizado. Actualizando recordatorios...")
+    bot.send_message(
+        chat_id=cid, text="Parseo finalizado. Actualizando recordatorios..."
+    )
 
     file_path = "./files/reminders.json"
-    with open(file_path, 'r') as f:
+    with open(file_path, "r") as f:
         res = json.load(f)
         for key, value in recordatorios.items():
             if key not in res:
@@ -111,7 +129,7 @@ def actualizar_json(message, recordatorios):
                 lista = list(value[0])
                 if lista not in res[key]:
                     res[key].extend(value)
-    
+
     claves = list(res.keys())
     fechas = []
     for c in claves:
@@ -120,12 +138,15 @@ def actualizar_json(message, recordatorios):
     claves = []
     for f in fechas:
         claves.append(f.strftime("%d/%m/%Y"))
-    res2 = {i:res[i] for i in claves}
-    
-    with open(file_path, 'w') as f:
+    res2 = {i: res[i] for i in claves}
+
+    with open(file_path, "w") as f:
         json.dump(res2, f)
-    
-    bot.send_message(chat_id=cid, text="Todos los recordatorios actualizados. Espero que tengas un día estupendo <3")
+
+    bot.send_message(
+        chat_id=cid,
+        text="Todos los recordatorios actualizados. Espero que tengas un día estupendo <3",
+    )
     return
 
 
--- reminder.py
+++ reminder.py
@@ -2,61 +2,63 @@ import os
 import telebot
 import json
 from dotenv import load_dotenv
-from datetime import date, datetime, timedelta
+from datetime import date, datetime
 
 load_dotenv()
 
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 bot = telebot.TeleBot(BOT_TOKEN)
 
 today = date.today()
 cid = -1001882486742
 aviso = "*IMPORTANTE*\n\nToca extender la expiración en PythonAnywhere si queréis que esto siga funcionando"
 
+
 def manda_mensaje():
-    #bot.send_message(cid, text="esto es una prueba de recordatorio")
+    # bot.send_message(cid, text="esto es una prueba de recordatorio")
 
     file_path = "./files/reminders.json"
-    with open(file_path, 'r') as f:
+    with open(file_path, "r") as f:
         recordatorios = json.load(f)
-        k,v = list(recordatorios.items())[0]
+        k, v = list(recordatorios.items())[0]
         fecha = datetime.strptime(k, "%d/%m/%Y").date()
-        
+
         if fecha == today:
             for r in v:
                 mensaje = r[1]
                 usuarios = r[2]
                 users = ""
                 for u in usuarios:
-                    users = users + "@"+u + " "
+                    users = users + "@" + u + " "
                 texto = users + "\n\n" + mensaje
                 bot.send_message(cid, texto)
             del recordatorios[k]
-        
+
         else:
             texto = "No hay ningún recordatorio para hoy. Espero que tengáis un gran día! <3"
             bot.send_message(cid, texto)
-    
-    with open(file_path, 'w') as f:
+
+    with open(file_path, "w") as f:
         json.dump(recordatorios, f)
 
     return
 
+
 def comprueba_fecha_task():
-    '''
+    """
     AVISAR DEL EXPIRY EL 1 Y EL 17 DE CADA MES
-    '''
+    """
     dia = today.day
     print(dia)
-    if dia==1 or dia==17:
-        bot.send_message(cid, aviso, parse_mode='Markdown')
+    if dia == 1 or dia == 17:
+        bot.send_message(cid, aviso, parse_mode="Markdown")
     return
-
 
 
 def main():
     manda_mensaje()
     comprueba_fecha_task()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
